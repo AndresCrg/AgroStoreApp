e04a179af3f18e8e400c8a8a2b4e8a619b4d077e
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-07-23 15:57:41 -0500

    AS-1 Se crea estructura básica del proyecto. Se crea el server en el puerto 4001

---
+++ src/models/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+class Server {
+
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT
+    }
+
+    listen(){
+        this.app.listen(this.port,() =>{
+            console.log(`Server listening on port ${this.port}`);
+        })
+    }
+}
+
+module.exports = Server
