877e681dd2477eb33ee5780452c64e4602efcee1
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-07-23 22:29:49 -0500

    AS-1 Se validan datos de entrada respecto a la bd. Ya se actualiza el email. ya se puede eliminar cuenta

---
+++ src/middlewares/validate-db.js
@@ -0,0 +1,58 @@
+const { prisma } = require('../controllers/user-controller');
+
+/**
+ *
+ * Employees
+ */
+
+const documentExist = async (document = '') => {
+	const existDocument = await prisma.user.findFirst({ where: { document } });
+	if (existDocument) {
+		throw new Error(`El documento: ${document}, ya está registrado!`);
+	}
+};
+
+const documentExistById = async (document = '') => {
+	const existDocument = await prisma.user.findUnique({
+		where: { document },
+	});
+	if (!existDocument) {
+		throw new Error(`El documento ${document} no esta registrado!`);
+	}
+};
+
+const emailExist = async (email = '') => {
+	const existEmail = await prisma.user.findFirst({ where: { email } });
+	if (existEmail) {
+		throw new Error(`El correo: ${email}, ya está registrado!`);
+	}
+};
+
+/**
+ *
+ * Credential
+ */
+
+const emailExistCredential = async (email = '') => {
+	const existEmail = await prisma.credential.findFirst({ where: { email } });
+	if (existEmail) {
+		throw new Error(`El correo: ${email}, ya está registrado!`);
+	}
+};
+
+const emailDoesNotRegistry = async (email = '') => {
+	const existEmail = await prisma.crendential.findFirst({
+		where: { email },
+	});
+	if (!existEmail) {
+		throw new Error(`Correo o contraseña incorrectas - Correo`);
+	}
+};
+
+module.exports = {
+	documentExist,
+	emailExist,
+	documentExistById,
+	emailDoesNotRegistry,
+	emailExistCredential,
+};
