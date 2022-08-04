489df7f8b02b20616a0c24e9675b086631c27b6f
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-08-03 19:50:12 -0500

    AS-5 Se validan los campos al momento de crear un producto

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,8 +1,25 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { createProduct } = require('../controllers/product-controller');
+const { validateFields } = require('../middlewares/validate-filelds');
 
 const router = Router();
 
-router.post('/', createProduct);
+router.post(
+	'/',
+	[
+		check('name', 'El nombre es obligatorio').not().isEmpty(),
+		check('type', 'El tipo de cultivo no es valido!').isIn(['FRUITS', 'CEREALS', 'LEGOMINOUS', 'VEGETABLES', 'TUBERS']),
+		check('harvestDate', 'No es formato de fecha válido').not().isEmpty().isDate(),
+		check('harvestLocation', 'La ubicación del cultivo es obligatorio!').not().isEmpty(),
+		check('units', 'Unidades de cultivo no válido').isIn(['KG', 'LB', 'AR']),
+		check('quantityAvalible', 'La cantidad disponible es obligatoria!').not().isEmpty(),
+		check('pricePerunit', 'El precio por unidad es obligatorio!').not().isEmpty(),
+		check('image', 'La imagen es obligatoria!').not().isEmpty(),
+		check('description', 'La descripción es obligatoria!').not().isEmpty(),
+		validateFields,
+	],
+	createProduct
+);
 
 module.exports = router;
